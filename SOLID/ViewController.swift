1ae35b1ce1716ca4d05135a5a0f613acb1b0d2f3
Valery <ihValery@gmail.com>
2021-03-24 10:12:25 +0300

    Initial Commit

---
+++ SOLID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLID
+//
+//  Created by Валерий Игнатьев on 24.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
